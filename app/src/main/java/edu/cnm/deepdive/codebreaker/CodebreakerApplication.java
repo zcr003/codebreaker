36658911c641ea0c017849d1f2ea4d26fa75ae3b
zcr003 <zramsey1@cnm.edu>
2021-10-14 11:04:06 -0600

    Added base_url to generated buildconfig.

---
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/CodebreakerApplication.java
@@ -0,0 +1,21 @@
+package edu.cnm.deepdive.codebreaker;
+
+import android.app.Application;
+import com.facebook.stetho.Stetho;
+
+//Our Application Class
+public class CodebreakerApplication extends Application {
+
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    Stetho.initializeWithDefaults(this);
+    // TODO Initialize database
+    //  (as well as any other necessary resources)
+  }
+
+
+
+
+}
