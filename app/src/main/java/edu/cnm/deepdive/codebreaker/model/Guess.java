77553fb0ca8eaed523221404593f74a5a926b2a1
zcr003 <zramsey1@cnm.edu>
2021-11-01 10:00:25 -0600

    Added entity features to DTO classes.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/model/Guess.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/model/Guess.java
@@ -1,31 +1,86 @@
 package edu.cnm.deepdive.codebreaker.model;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.Ignore;
+import androidx.room.Index;
+import androidx.room.PrimaryKey;
 import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
+
+@Entity(
+    tableName = "guess",
+    indices = {
+        @Index(value = "service_key", unique = true)
+    },
+    foreignKeys = {
+        @ForeignKey(
+            entity = Game.class,
+            parentColumns = "game_id",
+            childColumns = "game_id",
+            onDelete = ForeignKey.CASCADE
+        )
+    }
+)
 public class Guess {
 
+  @PrimaryKey
+  @ColumnInfo(name = "guess_id")
+  private long id;
+
+  @ColumnInfo(name = "game_id", index = true)
+  private long gameId;
+
+
+
+  @NonNull
   @Expose
-  private String id;
+  @SerializedName("id")
+  @ColumnInfo(name = "service_key")
+  private String serviceKey;
 
   @Expose
   private String text;
 
   @Expose
+  @ColumnInfo(name = "exact_matches")
   private int exactMatches;
 
   @Expose
+  @ColumnInfo(name = "near_matches")
   private int nearMatches;
 
+  @Ignore
   @Expose
   private boolean solution;
 
   //Getters and setters to give access outside of this class
-  public String getId() {
+  public long getId() {
     return id;
   }
 
-  public void setId(String id) {
+  public void setId(long id) {
     this.id = id;
+  }
+
+  public long getGameId() {
+    return gameId;
+  }
+
+  public void setGameId(long gameId) {
+    this.gameId = gameId;
+  }
+
+  @NonNull
+  public String getServiceKey() {
+    return serviceKey;
+  }
+
+  public void setServiceKey(@NonNull String serviceKey) {
+    this.serviceKey = serviceKey;
   }
 
   public String getText() {
