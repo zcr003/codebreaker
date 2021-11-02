c164c4b9889f2710d1b7c8a5bfe0f07031bbbac8
zcr003 <zramsey1@cnm.edu>
2021-11-02 17:08:33 -0600

    End of day commit.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/viewmodel/PlayViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/viewmodel/PlayViewModel.java
@@ -15,21 +15,16 @@ import edu.cnm.deepdive.codebreaker.service.GameRepository;
 import io.reactivex.disposables.CompositeDisposable;
 import java.util.List;
 
-public class MainViewModel extends AndroidViewModel implements LifecycleObserver {
+public class PlayViewModel extends AndroidViewModel implements LifecycleObserver {
 
   private final GameRepository repository;
   //Live data is not a requirement of viewmodel but will typically be there.
   private final MutableLiveData<Game> game;
-  //let the ui controller invoke a method that gives us a value for these 3 fields
-//  private final LiveData<List<GameSummary>> scores;
-//  private final MutableLiveData<Integer> poolSize;
-//  private final MutableLiveData<Integer> length;
-//  private final MutableLiveData<Boolean> orderByTotalTime;
   private final MutableLiveData<Throwable> throwable;
   private final CompositeDisposable pending;
 
   //The constructor to initialize. Always has the same name as the class. No return type.
-  public MainViewModel(@NonNull Application application) {
+  public PlayViewModel(@NonNull Application application) {
     super(application);
     repository = new GameRepository();
     game = new MutableLiveData<>();
