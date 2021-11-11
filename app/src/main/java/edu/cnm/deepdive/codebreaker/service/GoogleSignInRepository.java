11b0f09354600f69bb7bd71a56d44f20b9244570
zcr003 <zramsey1@cnm.edu>
2021-11-11 14:01:18 -0700

    Push before ProDev.

---
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/service/GoogleSignInRepository.java
@@ -0,0 +1,68 @@
+package edu.cnm.deepdive.codebreaker.service;
+
+import android.app.Application;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import edu.cnm.deepdive.codebreaker.BuildConfig;
+import io.reactivex.Single;
+import io.reactivex.SingleOnSubscribe;
+import io.reactivex.schedulers.Schedulers;
+
+public class GoogleSignInRepository {
+
+  private static final String BEARER_TOKEN_FORMAT = "Bearer %s";
+
+  private static Application context;
+
+  private final GoogleSignInClient client;
+
+  private GoogleSignInAccount account;
+
+  private GoogleSignInRepository() {
+    GoogleSignInOptions options = new GoogleSignInOptions.Builder()
+        .requestEmail()
+        .requestId()
+        .requestProfile()
+//        .requestIdToken(BuildConfig.CLIENT_ID)
+        .build();
+    client = GoogleSignIn.getClient(context, options);
+  }
+
+  public static void setContext(Application context) {
+    GoogleSignInRepository.context = context;
+  }
+
+  public static GoogleSignInRepository getInstance() {
+    return InstanceHolder.INSTANCE;
+  }
+
+  public Single<GoogleSignInAccount> refresh() {
+    return Single
+        .create((SingleOnSubscribe<GoogleSignInAccount>) (emitter) ->
+            client
+                .silentSignIn()
+                .addOnSuccessListener(this :: setAccount)
+                .addOnSuccessListener(emitter :: onSuccess)
+                .addOnFailureListener(emitter :: onError)
+        )
+        .observeOn(Schedulers.io());
+
+
+  }
+
+  private void setAccount(GoogleSignInAccount account) {
+    this.account = account;
+  }
+
+  private String getBearToken(GoogleSignInAccount account) {
+    return String.format(BEARER_TOKEN_FORMAT, account.getIdToken());
+  }
+
+  private static class InstanceHolder {
+
+    private static final GoogleSignInRepository INSTANCE = new GoogleSignInRepository();
+  }
+
+}
