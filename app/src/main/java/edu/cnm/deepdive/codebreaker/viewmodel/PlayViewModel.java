709b17263ef912a32f7b9e17a30edcccfcf135da
zcr003 <zramsey1@cnm.edu>
2021-11-22 17:02:33 -0700

    Emojis added to pool & new game starts once settings are selected.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/viewmodel/PlayViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/viewmodel/PlayViewModel.java
@@ -8,27 +8,38 @@ import androidx.lifecycle.Lifecycle.Event;
 import androidx.lifecycle.LifecycleObserver;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.OnLifecycleEvent;
+import edu.cnm.deepdive.codebreaker.R;
 import edu.cnm.deepdive.codebreaker.model.entity.Game;
 import edu.cnm.deepdive.codebreaker.model.entity.Guess;
 import edu.cnm.deepdive.codebreaker.service.GameRepository;
+import edu.cnm.deepdive.codebreaker.service.SettingsRepository;
 import io.reactivex.disposables.CompositeDisposable;
 
 public class PlayViewModel extends AndroidViewModel implements LifecycleObserver {
 
-  private final GameRepository repository;
+  private final GameRepository gameRepository;
+  private final SettingsRepository settingsRepository;
   //Live data is not a requirement of viewmodel but will typically be there.
   private final MutableLiveData<Game> game;
   private final MutableLiveData<Throwable> throwable;
   private final CompositeDisposable pending;
 
+  private int codeLength;
+  private int poolSize;
+  private String basePool;
+
+
   //The constructor to initialize. Always has the same name as the class. No return type.
   public PlayViewModel(@NonNull Application application) {
     super(application);
-    repository = new GameRepository();
+    gameRepository = new GameRepository();
+    settingsRepository = new SettingsRepository(application);
     game = new MutableLiveData<>();
     throwable = new MutableLiveData<>();
     pending = new CompositeDisposable();
-    startGame();
+    String[] emojis = application.getResources().getStringArray(R.array.emojis);
+    basePool = String.join("", emojis);
+    subscribeToSettings();
   }
 
   //Getters for throwable and game.
@@ -37,28 +48,33 @@ public class PlayViewModel extends AndroidViewModel implements LifecycleObserver
   }
 
 
-
   public MutableLiveData<Throwable> getThrowable() {
     return throwable;
   }
 
   //The Method. When we invoke startGame we get a single object.
   public void startGame() {
-    throwable.postValue(null);
-    Game game = new Game();
-    game.setPool("ABCDEF"); // TODO Read value from shared preferences.
-    game.setLength(3); // TODO Read value from shared preferences.
-    pending.add(
-        repository
-            .save(game)
-            .subscribe(
-                /* A consumer is a functional interface that takes a
-         parameter and doesn't return anything */
-                this.game::postValue,
-                this::postThrowable
+    if (codeLength > 0 && poolSize > 0) {
+      int[] poolCodePoints = basePool
+          .codePoints()
+          .limit(poolSize)
+          .toArray();
+      throwable.postValue(null);
+      Game game = new Game();
+      game.setPool(new String(poolCodePoints, 0, poolCodePoints.length));
+      game.setLength(codeLength);
+      pending.add(
+          gameRepository
+              .save(game)
+              .subscribe(
+                  /* A consumer is a functional interface that takes a
+           parameter and doesn't return anything */
+                  this.game::postValue,
+                  this::postThrowable
 
-            )
-    );
+              )
+      );
+    }
 
   }
 
@@ -69,7 +85,7 @@ public class PlayViewModel extends AndroidViewModel implements LifecycleObserver
     guess.setText(text);
     //noinspection ConstantConditions
     pending.add(
-        repository
+        gameRepository
             .save(game.getValue(), guess)
             .subscribe(
                 game::postValue,
@@ -91,4 +107,31 @@ public class PlayViewModel extends AndroidViewModel implements LifecycleObserver
     this.throwable.postValue(throwable);
 
   }
+
+  private void subscribeToSettings() {
+    pending.add(
+        settingsRepository
+            .getCodeLengthPreference()
+            .subscribe(
+                (codeLength) -> {
+                  this.codeLength = codeLength;
+                  startGame();
+                },
+                this::postThrowable
+            )
+    );
+    pending.add(
+        settingsRepository
+            .getPoolSizePreference()
+            .subscribe(
+                (poolSize) -> {
+                  this.poolSize = poolSize;
+                  startGame();
+                },
+                this::postThrowable
+            )
+    );
+
+  }
+
 }
